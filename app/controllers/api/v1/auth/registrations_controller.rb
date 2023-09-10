f1e6817bf92e52b77d040e114dc6dc379e2dc56a
satoru-tak <satoru.takaira@gmail.com>
2023-09-10 09:47:49 +0900

    Initial settings for devise_token_auth

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,2 +1,11 @@
-class Api::V1::Auth::RegistrationsController < ApplicationController
+class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  private
+
+  def sign_up_params
+    params.permit(:name, :email, :password, :password_confirm)
+  end
+
+  def account_update_params
+    params.permit(:name, :email, :password)
+  end
 end
--- db/migrate/20230909064132_devise_token_auth_create_users.rb
+++ db/migrate/20230909064132_devise_token_auth_create_users.rb
@@ -30,7 +30,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[7.0]
 
       ## User Info
       t.string :name
-      t.string :nickname
+      # t.string :nickname
       t.string :image
       t.string :email
 
