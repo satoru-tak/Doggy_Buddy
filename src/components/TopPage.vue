23187dd6e7b3b1c4c25d8683938faa8543bc24ed
satoru-tak <satoru.takaira@gmail.com>
2023-10-01 15:13:43 +0900

    topページ作成

--- src/components/TopPage.vue
+++ src/components/TopPage.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1>I'm TopPage</h1>
+  <div class="relative bg-cover h-[90vh] w-full" style="background-image: url('src/assets/top.jpg');">
+    <div class="text-white text-center absolute w-full p-[40vh]">
+      <p class="text-3xl">みんなが大切な家族</p>
+      <p class="text-base break-words">DoggyBuddyは、忙しくて愛犬のお世話が出来ない飼い主と
+      <br>近くに住んでる愛犬家を繋ぐドッグシッターサービスです</p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
